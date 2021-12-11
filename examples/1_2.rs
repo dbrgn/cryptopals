e6391d08276036739b5ce143c720249988301c25
Danilo Bargen <mail@dbrgn.ch>
2021-12-11 18:26:54 +0100

    Challenge 1.2

---
+++ examples/1_2.rs
@@ -0,0 +1,19 @@
+/// Set 1 challenge 1: Convert hex to base64
+use data_encoding::HEXLOWER;
+
+use cryptopals::fixed_xor_mut;
+
+fn main() {
+    let mut buf1 = HEXLOWER
+        .decode(b"1c0111001f010100061a024b53535009181c")
+        .unwrap();
+    let buf2 = HEXLOWER
+        .decode(b"686974207468652062756c6c277320657965")
+        .unwrap();
+    fixed_xor_mut(&mut buf1, &buf2);
+    assert_eq!(
+        HEXLOWER.encode(&buf1),
+        "746865206b696420646f6e277420706c6179"
+    );
+    println!("OK");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@ pub fn hex2base64(input: &str) -> String {
     let decoded: Vec<u8> = HEXLOWER.decode(input.as_bytes()).unwrap();
     BASE64.encode(&decoded)
 }
+
+/// XOR a with b in-place.
+pub fn fixed_xor_mut(a: &mut [u8], b: &[u8]) {
+    assert_eq!(a.len(), b.len());
+    for i in 0..a.len() {
+        a[i] ^= b[i];
+    }
+}
